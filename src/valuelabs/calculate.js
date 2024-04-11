5255c1e745d5c446b3c3399bf612a31e1cf10cf1
akshayarise <akshayawakearise@gmail.com>
2024-04-11 16:01:39 +0530

    default export example

---
+++ src/valuelabs/calculate.js
@@ -0,0 +1,5 @@
+// default export Example
+
+import sum from "./sum.js"
+
+console.log("sum is   ", sum(2, 3))
---
+++ src/valuelabs/sum.js
@@ -0,0 +1,5 @@
+// default export example
+
+export default function sum(a, b) {
+    return a + b;
+}
