288f87d3ddb33a256e892214a8da295e14cfeafa
akshayarise <akshayawakearise@gmail.com>
2024-04-11 13:22:54 +0530

    we have 3 ways to find out the power of the number in the Javascript

---
+++ src/valuelabs/power_of_the_number.js
@@ -0,0 +1,30 @@
+// How to find the 2 ^ n ? where n is the input.
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question(`What's your power of the number?\n`, power => {
+
+    // using loop
+    let num = 2;
+    for (let i = 1; i < power; i++) {
+        num = num * 2;
+    }
+    console.log("using for loop")
+    console.log(`power of the number is ${num}`)
+
+    //using Math.pow() method
+    console.log("using Math.pow() method")
+    console.log(Math.pow(2, power))
+
+    //using exponentiation method
+    console.log("using exponentiation method")
+    console.log(2 ** power)
+
+    rl.close();
+});
+
